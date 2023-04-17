1e4eeac04baa944df07cc7b23e001864759dfdf4
tanxin <txloveu@163.com>
2023-04-17 11:07:16 +0800

    chore: init project

---
+++ .eslintrc.cjs
@@ -0,0 +1,26 @@
+module.exports = {
+  settings: {
+    react: {
+      version: '18.2',
+    },
+  },
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  overrides: [],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint', 'prettier'],
+  rules: {},
+};
---
+++ .prettierrc.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+  printWidth: 100, // 每行代码长度（默认80）
+  tabWidth: 2, // 每个tab相当于多少个空格（默认2）ab进行缩进（默认false）
+  useTabs: false, // 是否使用
+  singleQuote: true, // 使用单引号（默认false）
+  semi: true, // 声明结尾使用分号(默认true)
+  bracketSpacing: true, // 对象字面量的大括号间使用空格（默认true）
+  jsxBracketSameLine: false, // 多行JSX中的>放置在最后一行的结尾，而不是另起一行（默认false）
+  arrowParens: 'avoid',
+}
